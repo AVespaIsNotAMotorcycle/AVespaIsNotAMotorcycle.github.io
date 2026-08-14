07dce111ca2e0bb82664c7cd75cdde83970ce763
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2026-08-13 17:35:09 -0700

    Update layout

--- app/page.tsx
+++ app/page.tsx
@@ -24,21 +24,49 @@ function Projects() {
 	)
 }
 
+const CONTACT = {
+	email: {
+		link: 'mailto:sasha.m.ebersole@gmail.com',
+		name: 'sasha.m.ebersole@gmail.com',
+	},
+	github: {
+		link: 'https://github.com/AVespaIsNotAMotorcycle',
+		name: 'GitHub'
+	},
+	linkedin: {
+		link: 'https://www.linkedin.com/in/sasha-ebersole-481a39257/',
+		name: "LinkedIn",
+	},
+};
+
+function Address() {
+	return (
+			<address>
+				{Object.keys(CONTACT).map((key) => (
+					<a key={key} href={CONTACT[key].link}>{CONTACT[key].name}</a>
+				))}
+			</address>
+	);
+}
+
 export default function Home() {
   return (
 		<main>
-			<h1>Sasha Madden Ebersole</h1>
+			<section>
+				<h1>Sasha Madden Ebersole</h1>
+				<Address />
+			</section>
 			<section>
 				<h2>About Sasha</h2>
 				<p>
-					Sasha is a current deli clerk and aspiring machine learning engineer.
-					In the past they have worked as a frontend developer at a startup
-					called DataMixMaster and as a full-stack developer at an IT contractor
-					called Groenewold IT Solutions. They speak fluent English and competent
-					German. Their Spanish isn't great. They have a Bachelor's in Computer
-					Science and History from Hunter College. Outside of work they like to
-					read about late imperial Chinese history, train Muay Thai, write poetry
-					and short fiction, and draw.
+					Sasha is a current deli clerk and aspiring machine learning engineer
+					living in Seattle, Washington. In the past they have worked as a
+					frontend developer at a startup, DataMixMaster, and as a full-stack
+					developer at an IT contractor, Groenewold IT Solutions. They speak
+					fluent English and competent German. Their Spanish isn't great. They
+					have a Bachelor's in Computer Science and History from Hunter College.
+					Outside of work they like to read about late imperial Chinese history,
+					train Muay Thai, write poetry and short fiction, and draw.
 				</p>
 			</section>
 			<section>
