3e6cedf1adba3a9f692e87b981ba9133dc8af4c7
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2026-08-15 20:16:47 -0700

    Broke About into own file

---
+++ app/About.tsx
@@ -0,0 +1,17 @@
+export default function About() {
+  return (
+		<section>
+			<h2>About Sasha</h2>
+			<p>
+				Sasha is a current deli clerk and aspiring machine learning engineer
+				living in Seattle, Washington. In the past they have worked as a
+				frontend developer at a startup, DataMixMaster, and as a full-stack
+				developer at an IT contractor, Groenewold IT Solutions. They speak
+				fluent English and competent German. Their Spanish isn't great. They
+				have a Bachelor's in Computer Science and History from Hunter College.
+				Outside of work they like to read about late imperial Chinese history,
+				train Muay Thai, write poetry and short fiction, and draw.
+			</p>
+		</section>
+  );
+}
